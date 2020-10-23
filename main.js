66b3f0263e607e761dfec7b098239b70549ab9da
leehyeokjae <leehyeokjae97@gmail.com>
2020-10-24 04:52:38 +0900

    성능 개선
    api request 횟수 감소
    불필요한 data정리
    주석 추가

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ function getFormatDate(date){
     month = month >= 10 ? month : '0' + month;  //month 두자리로 저장
     var day = date.getDate();                   //d
     day = day >= 10 ? day : '0' + day;          //day 두자리로 저장
-    return  year + '-' + month + '-' + day;       //'-' 추가하여 yyyy-mm-dd 형태 생성 가능
+    return  year + '-' + month + '-' + day;     //'-' 추가하여 yyyy-mm-dd 형태 생성 가능
+}
+
+function sum(array) {
+  var result = 0.0;
+
+  for (var i = 0; i < array.length; i++)
+    result += array[i];
+
+  return result;
 }
 
 function country1_help_on(){
@@ -34,142 +43,74 @@ var target_api = new Array();
 var countries_options = "";
 var countries_slug_list = new Array();
 var countries_name_list = new Array();
-var target_countries = new Array(6);
+var target_countries = new Array(3);
 
 
 var target_time_start = document.getElementById("target_time_start").value;
 var target_time_end = document.getElementById("target_time_end").value;
 var date1 = new Date(target_time_start);
 var date2 = new Date(target_time_end);
-var diffDay = (date2.getTime()-date1.getTime()) / (1000*60*60*24)+1;
+var diffDay = (date2.getTime()-date1.getTime()) / (1000*60*60*24)+1; //입력받은 Date의 차
 var date_item_x = date1;
-var date_x = ["date"];
-var target0_country_case = new Array();
-var target1_country_case = new Array();
-var target2_country_case = new Array();
-var target3_country_case = new Array();
-var target4_country_case = new Array();
-var target5_country_case = new Array();
-var targets_country_case = new Array();
-
-var target0_country_recover = new Array();
-var target0_country_deaths = new Array();
-
-
-for(i=1;i<=diffDay;i++){
-    date_item_x.setDate(date_item_x.getDate()+i-1)
-    date_x[i]=getFormatDate(date_item_x).substring(2,11);
-    date_item_x = new Date(target_time_start);
-}
+var date_x = ["date"]; //X축 값을 배정해줄 배열
 
 
 
+
+
+
+
+//나라별 slug값과 name값을 받아옴
 $.ajax({url:  country_api, dataType: "json",async: false,
         success: function(data){
             for(i=0;i<data.length;i++){
                 countries_slug_list[i]=data[i].Slug;
                 countries_name_list[i]=data[i].Country;
-                countries_options += '<option value="' + countries_name_list[i] + '">'
+                countries_options += '<option value="' + countries_name_list[i] + '">' //받아온 나라별 name을 HTML Option 형식으로 바꿔서 저장
             }
-            document.getElementById("countries").innerHTML = countries_options;
+            document.getElementById("countries").innerHTML = countries_options; // 저장한 옵션을 HTML에 입력
         }
     });
 
+    var targets_country_confirmed = new Array();
+var targets_country_active= new Array();
+var targets_country_deaths= new Array();
+var targets_country_recovered= new Array();
 
-for(i=0; i<6; i++){
-    target_countries[i] = countries_slug_list[countries_name_list.indexOf(document.getElementById("country"+i+"_input").value)];
-    target_api[i] = main_api + target_countries[i] + "/status/confirmed?from=" + target_time_start + "T00:00:00Z&to=" + target_time_end + "T00:00:00Z";
-    }
+    for(i=0; i<3;i++){
+      target_countries[i] = countries_slug_list[countries_name_list.indexOf(document.getElementById("country"+i+"_input").value)]; //입력받은 국가별 name값을 api를 받아올수 있는 slug값으로 변환해서 저장
+      target_api[i] = main_api + target_countries[i] + "?from=" + target_time_start + "T00:00:00Z&to=" + target_time_end + "T00:00:00Z";
+      $.ajax({url:  target_api[i], dataType: "json",async: false,
+      success: function(data){
+          var target_country_confirmed=[document.getElementById("country"+i+"_input").value]; //처음에 for문을 돌렸더니 마지막 ajax값만 들어가서 서로 다른 ajax을 여러번 사용했으나 저장할 배열을 ajax안에서 선언하니 해결됨
+          var target_country_active=[document.getElementById("country"+i+"_input").value];
+          var target_country_deaths=[document.getElementById("country"+i+"_input").value];
+          var target_country_recovered=[document.getElementById("country"+i+"_input").value];
+          for(k=1;k<=data.length;k++){
+              target_country_confirmed[k]=data[k-1].Confirmed;
+              target_country_active[k]=data[k-1].Active;
+              target_country_deaths[k]=data[k-1].Deaths;
+              target_country_recovered[k]=data[k-1].Recovered;
+          }
+          targets_country_confirmed[i]=target_country_confirmed;
+          targets_country_active[i]=target_country_active;
+          targets_country_deaths[i]=target_country_deaths;
+          targets_country_recovered[i]=target_country_recovered;
+          
+      }
+      
+  });
+    
+      }
 
-    $.ajax({url:  target_api[0], dataType: "json",async: false,
-        success: function(data){
-            target0_country_case[0]=target_countries[0]+"_cases";
-            for(k=1;k<=diffDay;k++){
-                target0_country_case[k]=data[k-1].Cases;
-            }
-            targets_country_case[0]=target0_country_case;
-        }
-    });
-    $.ajax({url:  target_api[1], dataType: "json",async: false,
-        success: function(data){
-            target1_country_case[0]=target_countries[1]+"_cases";
-            for(i=1;i<=diffDay;i++){
-                target1_country_case[i]=data[i-1].Cases;
-            }
-            targets_country_case[1]=target1_country_case;
-        }
-    });
-    $.ajax({url:  target_api[2], dataType: "json",async: false,
-        success: function(data){
-            target2_country_case[0]=target_countries[2]+"_cases";
-            for(i=1;i<=diffDay;i++){
-                target2_country_case[i]=data[i-1].Cases;
-            }
-            targets_country_case[2]=target2_country_case;
-        }
-    });
-    $.ajax({url:  target_api[3], dataType: "json",async: false,
-        success: function(data){
-            target3_country_case[0]=target_countries[3]+"_cases";
-            for(i=1;i<=diffDay;i++){
-                target3_country_case[i]=data[i-1].Cases;
-            }
-            targets_country_case[3]=target3_country_case;
-        }
-    });
-    $.ajax({url:  target_api[4], dataType: "json",async: false,
-        success: function(data){
-            target4_country_case[0]=target_countries[4]+"_cases";
-            for(i=1;i<=diffDay;i++){
-                target4_country_case[i]=data[i-1].Cases;
-            }
-            targets_country_case[4]=target4_country_case;
-        }
-    });
-    $.ajax({url:  target_api[5], dataType: "json",async: false,
-        success: function(data){
-            target5_country_case[0]=target_countries[5]+"_cases";
-            for(i=1;i<=diffDay;i++){
-                target5_country_case[i]=data[i-1].Cases;
-            }
-            targets_country_case[5]=target5_country_case;
-        }
-    });
-
-    $.ajax({url:  target_api[0].replace("/status/confirmed",""), dataType: "json",async: false,
-    success: function(data){
-        target0_country_recover[0]="Recovered";
-        target0_country_deaths[0]="Deaths";
-        for(i=1;i<=diffDay;i++){
-            target0_country_recover[i]=data[i-1].Recovered;
-            target0_country_deaths[i]=data[i-1].Deaths;
-        }
-    }
-});
-var world_new_confirmed = [];
-var world_new_deaths =[];
-var world_total_confirmed=0;
-var world_total_active=0;
-var world_total_recovered=0;
-var world_total_deaths=0;
-$.ajax({url:  "https://api.covid19api.com/world", dataType: "json",async: false,
-success: function(data){
-    world_new_confirmed[0]="Confirmed";
-    world_new_deaths[0]="Deaths";
-    world_total_deaths=data[data.length-1].TotalDeaths;
-    world_total_recovered=data[data.length-1].TotalRecovered;
-    world_total_confirmed=data[data.length-1].TotalConfirmed;
-    world_total_active=world_total_confirmed- world_total_recovered- world_total_deaths;
-    for(k=0,z=1;k<=data.length;z++){
-        for(i=0;i<10;i++){
-          world_new_confirmed[z]=data[k].NewConfirmed;
-          world_new_deaths[z]=data[k].NewDeaths;
-          k++;
-        }
-    }
+//그래프 X축에 들어갈 값들 입력
+for(i=1;i<=diffDay;i++){
+  date_item_x.setDate(date_item_x.getDate()+i-1)
+  date_x[i]=getFormatDate(date_item_x).substring(2,11);
+  date_item_x = new Date(target_time_start);
 }
-});
-document.getElementById("test").textContent=world_new_deaths[1]
+
+
         var chart1 = c3.generate({
     
     bindto: "#linechart",
@@ -178,12 +119,9 @@ document.getElementById("test").textContent=world_new_deaths[1]
     x : "date",
       columns: [
       date_x,
-      targets_country_case[0],
-      targets_country_case[1],
-      targets_country_case[2],
-      targets_country_case[3],
-      targets_country_case[4],
-      targets_country_case[5]
+      targets_country_confirmed[0],
+      targets_country_confirmed[1],
+      targets_country_confirmed[2],
       ]
   
     },
@@ -194,60 +132,27 @@ document.getElementById("test").textContent=world_new_deaths[1]
     }
   
   });
-
+for(i=0; i<3; i++){
   var chart2 = c3.generate({
-
-    bindto: "#areachart",
-  
-    data: {
-        x : "date",
-      columns: [
-        date_x,
-        targets_country_case[0],  
-  
-        target0_country_recover,
-  
-        target0_country_deaths
-  
-  ],
-  
-      types: {
-  
-        Recovered: 'area-spline',
-  
-        Deaths: 'area-spline',
-  
-      },
-      colors:{
-        Recovered:"#1DDB16",
-        Deaths:"#000000"
-      }
-  
-    },
-    axis:{
-        x : {
-            type:"timeseries"
-        }
-    },
-    point : {
-        show: false
-    }
-  
-  });
-  var chart3 = c3.generate({
-    bindto: "#worldchart1",
+    bindto: "#donutchart"+i,
     data: {
       columns: [
-        ['Active', world_total_active],
-        ['Deaths', world_total_deaths],
-        ['Recovered',world_total_recovered]
+        ["Active",targets_country_active[i][targets_country_active[i].length-1]],
+        ["Deaths",targets_country_deaths[i][targets_country_deaths[i].length-1]],
+        ["Recovered",targets_country_recovered[i][targets_country_recovered[i].length-1]]
       ],
       type: "donut",
+      colors:{Deaths:"#000000",Recovered:"#47C83E",Active:"#FF0000"},
       labels: {
         format: {
             y: d3.format(".1%"),
         }}
     },
+    donut: {
+
+      expand: false,
+  
+      title: document.getElementById("country"+i+"_input").value},
     axis: {
       x: {
         type: 'categorized',
@@ -262,68 +167,7 @@ document.getElementById("test").textContent=world_new_deaths[1]
     }
   });
 
-  var chart3 = c3.generate({
-    bindto: "#worldchart2",
-    data: {
-      columns: [
-        ['Active', world_total_active],
-        ['Deaths', world_total_deaths],
-        ['Recovered',world_total_recovered]
-      ],
-      type: "donut",
-      labels: {
-        format: {
-            y: d3.format(".1%"),
-        }}
-    },
-    axis: {
-      x: {
-        type: 'categorized',
-        categories: ['Unique Click Rate','Total Click Rate']
-      },
-    },
-    bar: {
-      width: {
-        ratio: 0.5,
+}
+  
 
-      },
-    }
-  });
-
-  var chart4 = c3.generate({
-
-    bindto: "#worldchart3",
-  
-    data: {
-      columns: [
-        world_new_deaths,
-        world_new_confirmed
-  
-  ],
-  
-      types: {
-  
-        Confirmed: 'area-spline',
-  
-        Deaths: 'area-spline',
-  
-      },
-      colors:{
-        Confirmed:"RED",
-        Deaths:"#000000"
-      }
-  
-    },
-    axis:{
-        y : {
-            max: 300000,
-            min: 0,
-            padding: {top:0,bottom:0}
-        }
-    },
-    point : {
-        show: false
-    }
-  
-  });
 }
