c9a143a387aad34ad2ed35679e4dae9e75c7c81b
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-07-03 20:37:39 +0900

    [CREATE] Worker
    Create worker

---
+++ src/js/worker.js
@@ -0,0 +1,16 @@
+const API_URL = "https://korea-covid19-api.herokuapp.com/";
+onmessage = async (e) => {
+  const data = await getGraphQL(API_URL, e.data);
+  postMessage(data);
+};
+
+const getGraphQL = async (url, query) => {
+  const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query }),
+    }),
+    json = await res.json(),
+    data = JSON.stringify(json.data);
+  return data;
+};
