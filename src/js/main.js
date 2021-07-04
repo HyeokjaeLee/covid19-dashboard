0110450887fe09106e8d8b0b0d262539843cc25e
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-07-04 14:07:40 +0900

    [UPDATE] Worker Code

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,61 @@
 const create_query = (region, startDate, endDate) => `query{
-  covid19Info(region:"${region}"){
+  covid19Info(region:${region} startDate:${startDate} endDate:${endDate}){
     regionEng
+    regionKor
+    population
     covid19{
       date
+      confirmed{
+        total
+        accumlated
+      }
+      quarantine{
+        total
+        new{
+          total
+          domestic
+          overseas
+        }
+      }
+      recovered{
+        total
+        new
+        accumlated
+      }
+      dead{
+        total
+        new
+        accumlated
+      }
+      vaccination{
+        first{
+          total
+          new
+          accumlated
+        }
+        second{
+          total
+          new
+          accumlated
+        }
+      }
+      per100kConfirmed
     }
   }
 }`;
 
-const query = `query{
-  covid19Info(region:"Seoul"){
-    regionEng
-    covid19{
-      date
-    }
-  }
-}`;
+const get_API_data = () => {
+  return new Promise((resolve, reject) => {
+    const APIworker = new Worker("/src/js/worker.js");
+    APIworker.postMessage(create_query("Seoul", 20210101, 20210201));
+    APIworker.onmessage = (e) => {
+      resolve(e.data);
+    };
+  });
+};
 
 const test = async () => {
-  const APIworker = new Worker("/src/js/worker.js");
-  APIworker.postMessage(create_query("Seoul"));
-  APIworker.onmessage = (e) => {
-    console.log(e.data);
-    APIworker.terminate();
-  };
+  console.log(await get_API_data());
 };
 
 test();
